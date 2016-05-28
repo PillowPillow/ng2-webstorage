cf6f1087444ba0d3d746fc2046565770f4e741e6
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2016-05-28 23:53:42 +0200

    add webstorage service

---
+++ src/services/webStorage.ts
@@ -0,0 +1,36 @@
+import {EventEmitter} from '@angular/core';
+import {KeyStorageHelper, WebStorageHelper, StorageObserverHelper} from '../helpers';
+import {IStorage} from '../interfaces';
+import {STORAGE} from '../enums/storage';
+
+export class WebStorage implements IStorage {
+
+	private sType:STORAGE = null;
+
+	constructor(sType:STORAGE) {
+		this.sType = sType;
+	}
+
+	public store(raw:string, value:string):void {
+		let sKey = KeyStorageHelper.genKey(raw);
+		WebStorageHelper.store(this.sType, sKey, value);
+	}
+
+	public retrieve(raw:string):string {
+		let sKey = KeyStorageHelper.genKey(raw);
+		return WebStorageHelper.retrieve(this.sType, sKey);
+	}
+
+	public clear(raw?:string):void {
+		if(raw)
+			WebStorageHelper.clear(this.sType, KeyStorageHelper.genKey(raw));
+		else
+			WebStorageHelper.clearAll(this.sType);
+	}
+
+	public observe(raw:string):EventEmitter {
+		let sKey = KeyStorageHelper.genKey(raw);
+		return StorageObserverHelper.observe(this.sType, sKey)
+	}
+
+}
