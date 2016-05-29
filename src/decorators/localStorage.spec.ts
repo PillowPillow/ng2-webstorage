fff7ac4ec04bc43cb7c908c62f8d0b958af374a7
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2016-05-29 03:10:05 +0200

    add localstorage decorator unit tests

---
+++ src/decorators/localStorage.spec.ts
@@ -0,0 +1,61 @@
+import {beforeEachProviders, describe, inject, injectAsync, it} from '@angular/core/testing';
+import {Component} from '@angular/core';
+import {LocalStorage} from './localStorage';
+import {LocalStorageService} from '../services/localStorage';
+
+
+@Component({
+	selector: 'test',
+	template: ``
+})
+class TestComponent {
+
+	@LocalStorage()
+	local_key1 = 'default value';
+
+	@LocalStorage('local_key2')
+	key;
+
+}
+
+describe('decorator:localStorage', () => {
+
+	beforeEachProviders(() => [TestComponent, LocalStorageService]);
+
+	it('should keep the default value', inject([TestComponent], (component) => {
+		expect(component.local_key1).toEqual('default value');
+	}));
+
+	it('should update the local_key1 value by the wstorage service', inject([TestComponent, LocalStorageService], (component, localS) => {
+
+		let value = 'new value';
+		localS.store('local_key1', value);
+		expect(component.local_key1).toEqual(value);
+
+	}));
+
+	it('should update the local_key1 value by the decorator', inject([TestComponent, LocalStorageService], (component, localS) => {
+
+		let value = 'new value';
+		component.local_key1 = value;
+		expect(localS.retrieve('local_key1')).toEqual(value);
+
+	}));
+
+	it('should update the local_key2 value by the wstorage service', inject([TestComponent, LocalStorageService], (component, localS) => {
+
+		let value = 'new value';
+		localS.store('local_key2', value);
+		expect(component.key).toEqual(value);
+
+	}));
+
+	it('should update the local_key2 value by the decorator', inject([TestComponent, LocalStorageService], (component, localS) => {
+
+		let value = 'new value';
+		component.key = value;
+		expect(localS.retrieve('local_key2')).toEqual(value);
+
+	}));
+
+});
