27e4d98c508fcd4ad3c1012811c3b3e5d1691ec2
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2016-05-29 02:51:16 +0200

    add storage observe helper unit tests

---
+++ src/helpers/storageObserver.spec.ts
@@ -0,0 +1,64 @@
+import {beforeEachProviders, describe, inject, injectAsync, it} from '@angular/core/testing';
+import {EventEmitter} from '@angular/core';
+
+import {StorageObserverHelper} from './storageObserver';
+import {LIB_KEY} from '../constants/lib';
+import {STORAGE} from '../enums/storage';
+
+describe('helper:storageObserver', () => {
+
+	describe('observe', () => {
+
+		let event = StorageObserverHelper.observe(STORAGE.local, 'storageObserver:key');
+
+		it('should return an EventEmitter instance', () => {
+
+			expect(event instanceof EventEmitter).toBe(true);
+
+		});
+
+		it('should return the same EventEmitter instance', () => {
+
+			expect(StorageObserverHelper.observe(STORAGE.local, 'storageObserver:key') === event).toBe(true);
+
+		});
+
+		it('should return an other EventEmitter instance', () => {
+
+			expect(StorageObserverHelper.observe(STORAGE.local, 'storageObserver:key2') !== event).toBe(true);
+
+		});
+
+	});
+
+
+	describe('emit', () => {
+
+		let event = StorageObserverHelper.observe(STORAGE.local, 'storageObserver:key');
+
+		it('should emit an event to the subscribers', (done) => {
+
+			let result = 'emitted';
+
+			let sub = event.subscribe((data) => {
+				expect(data).toEqual(result);
+				done();
+			});
+
+			event.emit('emitted');
+		});
+
+	});
+
+	describe('genObserverKey', () => {
+
+		it('should return a storage key', () => {
+
+			let sKey = 'key';
+			expect(StorageObserverHelper.genObserverKey(STORAGE.local, sKey)).toEqual(`${STORAGE.local}|${sKey}`);
+
+		});
+
+	});
+
+});
