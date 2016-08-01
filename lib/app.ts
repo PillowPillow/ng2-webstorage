a853b3ed3b32ae45bccde269a51dee7ef6c71b39
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2016-08-01 10:45:54 +0200

    add keystorage helper to the exposed services
    provides a way to update the prefix used by the library

--- lib/app.ts
+++ lib/app.ts
@@ -2,6 +2,7 @@ import {LocalStorageService, SessionStorageService} from './services/index';
 export * from './decorators/index';
 export * from './interfaces/index';
 export * from './services/index';
+export * from './helpers/keyStorage';
 
 export const NG2_WEBSTORAGE = [
 	SessionStorageService,
