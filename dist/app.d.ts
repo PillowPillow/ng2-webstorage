9eb2b1f1bbb0534d1b4dff9260c93c9d2c97e033
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2016-09-28 21:43:52 +0200

    update dist version

--- dist/app.d.ts
+++ dist/app.d.ts
@@ -1,6 +1,6 @@
-import { SessionStorageService } from './services/index';
 export * from './decorators/index';
 export * from './interfaces/index';
 export * from './services/index';
 export * from './helpers/keyStorage';
-export declare const NG2_WEBSTORAGE: typeof SessionStorageService[];
+export declare class Ng2Webstorage {
+}
