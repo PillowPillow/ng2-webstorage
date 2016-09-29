200f618def2d2febd507780cbe0438eab01681c9
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2016-09-29 18:04:13 +0200

    add fallback strategy IOS/safary private mode #11

--- lib/constants/lib.ts
+++ lib/constants/lib.ts
@@ -1,1 +1,7 @@
+import {STORAGE} from '../enums/storage';
+
 export const LIB_KEY = 'ng2-webstorage';
+export const STORAGE_NAMES = {
+	[STORAGE.local]: 'local',
+	[STORAGE.session]: 'session',
+};
--- lib/helpers/webStorage.ts
+++ lib/helpers/webStorage.ts
@@ -2,33 +2,35 @@ import {IWebStorage} from '../interfaces/webStorage';
 import {STORAGE} from '../enums/storage';
 import {StorageObserverHelper} from './storageObserver';
 import {KeyStorageHelper} from './keyStorage';
+import {MockStorageHelper} from './mockStorage';
+import {STORAGE_NAMES} from '../constants/lib';
 
 export class WebStorageHelper {
 
 	static cached = {[STORAGE.local]: {}, [STORAGE.session]: {}};
+	static storageAvailability = {[STORAGE.local]: null, [STORAGE.session]: null};
 
-	static store(sType:STORAGE, sKey:string, value:any):void {
-		this.getWStorage(sType).setItem(sKey, JSON.stringify(value));
+	static store(sType: STORAGE, sKey: string, value: any): void {
+		this.getStorage(sType).setItem(sKey, JSON.stringify(value));
 		this.cached[sType][sKey] = value;
 		StorageObserverHelper.emit(sType, sKey, value);
 	}
 
-	static retrieve(sType:STORAGE, sKey:string):string {
-		if(sKey in this.cached[sType])
-			return this.cached[sType][sKey];
+	static retrieve(sType: STORAGE, sKey: string): string {
+		if(this.cached[sType][sKey]) return this.cached[sType][sKey];
 
 		let data = null;
 		try {
-			data = JSON.parse(this.getWStorage(sType).getItem(sKey));
+			data = JSON.parse(this.getStorage(sType).getItem(sKey));
 		} catch(err) {
-			console.error(`invalid value for ${sKey}`);
+			console.warn(`invalid value for ${sKey}`);
 		}
 
 		return this.cached[sType][sKey] = data;
 	}
 
-	static clearAll(sType:STORAGE):void {
-		let storage:IWebStorage = this.getWStorage(sType);
+	static clearAll(sType: STORAGE): void {
+		let storage: IWebStorage = this.getStorage(sType);
 		KeyStorageHelper.retrieveKeysFromStorage(storage)
 		.forEach((sKey) => {
 			storage.removeItem(sKey);
@@ -37,13 +39,17 @@ export class WebStorageHelper {
 		});
 	}
 
-	static clear(sType:STORAGE, sKey:string):void {
-		this.getWStorage(sType).removeItem(sKey);
+	static clear(sType: STORAGE, sKey: string): void {
+		this.getStorage(sType).removeItem(sKey);
 		delete this.cached[sType][sKey];
 		StorageObserverHelper.emit(sType, sKey, null);
 	}
 
-	static getWStorage(sType:STORAGE):IWebStorage {
+	static getStorage(sType: STORAGE): IWebStorage {
+		return this.isStorageAvailable(sType) ? this.getWStorage(sType) : MockStorageHelper.getStorage(sType);
+	}
+
+	static getWStorage(sType: STORAGE): IWebStorage {
 		let storage;
 		switch(sType) {
 			case STORAGE.local:
@@ -56,6 +62,26 @@ export class WebStorageHelper {
 				throw Error('invalid storage type');
 		}
 		return storage;
+	}
+
+	static isStorageAvailable(sType: STORAGE) {
+		if(typeof this.storageAvailability[sType] === 'boolean')
+			return this.storageAvailability[sType];
+
+		let isAvailable = true, storage = this.getWStorage(sType);
+
+		if(typeof storage === 'object') {
+			try {
+				storage.setItem('test-storage', 'foobar');
+				storage.removeItem('test-storage');
+			} catch(e) {
+				isAvailable = false;
+			}
+		}
+		else isAvailable = false;
+
+		if(!isAvailable) console.warn(`${STORAGE_NAMES[sType]} storage unavailable, Ng2Webstorage will use a fallback strategy instead`);
+		return this.storageAvailability[sType] = isAvailable;
 	}
 
 }
