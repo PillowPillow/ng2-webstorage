bac9ed878909e4c47158363aada0982d8e6ef10a
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2017-04-21 11:34:27 +0200

    add case sensitive constant

--- lib/constants/lib.ts
+++ lib/constants/lib.ts
@@ -2,6 +2,7 @@ import {STORAGE} from '../enums/storage';
 
 export const LIB_KEY = 'ng2-webstorage';
 export const LIB_KEY_SEPARATOR = '|';
+export const LIB_KEY_CASE_SENSITIVE = false;
 
 export const STORAGE_NAMES = {
 	[STORAGE.local]: 'local',
