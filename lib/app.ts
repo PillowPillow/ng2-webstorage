bf3dc8dfb0ab6ad887562ba07669650093c6100d
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2017-04-21 11:34:52 +0200

    update configuration - add case sensitive option

--- lib/app.ts
+++ lib/app.ts
@@ -1,5 +1,5 @@
 import {NgModule, NgZone, ModuleWithProviders, OpaqueToken, Inject, Optional} from '@angular/core';
-import {LIB_KEY, LIB_KEY_SEPARATOR} from './constants/lib';
+import {LIB_KEY, LIB_KEY_SEPARATOR, LIB_KEY_CASE_SENSITIVE} from './constants/lib';
 import {STORAGE} from './enums/storage';
 import {LocalStorageService, SessionStorageService} from './services/index';
 import {WebStorageHelper} from './helpers/webStorage';
@@ -42,6 +42,7 @@ export class Ng2Webstorage {
 		if(config) {
 			KeyStorageHelper.setStorageKeyPrefix(config.prefix);
 			KeyStorageHelper.setStorageKeySeparator(config.separator);
+			KeyStorageHelper.setCaseSensitivity(config.caseSensitive);
 		}
 
 		this.initStorageListener();
@@ -61,10 +62,15 @@ export function provideConfig(config:IWebstorageConfig):WebstorageConfig {
 	return new WebstorageConfig(config);
 }
 
-export function configure({prefix, separator}:IWebstorageConfig = {prefix: LIB_KEY, separator: LIB_KEY_SEPARATOR}) {
+export function configure({prefix, separator, caseSensitive}:IWebstorageConfig = {
+	caseSensitive: LIB_KEY_CASE_SENSITIVE,
+	prefix: LIB_KEY,
+	separator: LIB_KEY_SEPARATOR
+}) {
 	/*@Deprecation*/
 	console.warn('[ng2-webstorage:deprecation] The configure method is deprecated since the v1.5.0, consider to use forRoot instead');
 
 	KeyStorageHelper.setStorageKeyPrefix(prefix);
 	KeyStorageHelper.setStorageKeySeparator(separator);
+	KeyStorageHelper.setCaseSensitivity(caseSensitive);
 }
--- lib/interfaces/config.ts
+++ lib/interfaces/config.ts
@@ -1,21 +1,27 @@
-import {LIB_KEY, LIB_KEY_SEPARATOR} from '../constants/lib';
+import {LIB_KEY, LIB_KEY_CASE_SENSITIVE, LIB_KEY_SEPARATOR} from '../constants/lib';
 
 export interface IWebstorageConfig {
-	prefix?: string;
-	separator?: string;
+	prefix?:string;
+	separator?:string;
+	caseSensitive?:boolean;
 }
 
 export class WebstorageConfig implements IWebstorageConfig {
-	prefix: string = LIB_KEY;
-	separator: string = LIB_KEY_SEPARATOR;
+	prefix:string = LIB_KEY;
+	separator:string = LIB_KEY_SEPARATOR;
+	caseSensitive:boolean = LIB_KEY_CASE_SENSITIVE;
 
-	constructor(config?: IWebstorageConfig) {
-		if (config && config.prefix !== undefined) {
+	constructor(config?:IWebstorageConfig) {
+		if(config && config.prefix !== undefined) {
 			this.prefix = config.prefix;
 		}
 
-		if (config && config.separator !== undefined) {
+		if(config && config.separator !== undefined) {
 			this.separator = config.separator;
+		}
+
+		if(config && config.caseSensitive !== undefined) {
+			this.caseSensitive = config.caseSensitive;
 		}
 	}
 }
