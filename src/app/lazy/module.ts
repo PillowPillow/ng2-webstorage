e2630e01fc674bc4db58c01420a68b1ebb2c80fa
PillowPillow <gaignoux.nicolas@gmail.com>
2018-10-24 15:19:21 +0200

    remove module ref in the lazy component

--- src/app/lazy/module.ts
+++ src/app/lazy/module.ts
@@ -7,8 +7,7 @@ import {NgxWebstorageModule} from '../lib';
 @NgModule({
 	imports: [
 		SharedModule,
-		Routing,
-		NgxWebstorageModule.forRoot()
+		Routing
 	],
 	exports: [],
 	declarations: [...Components],
