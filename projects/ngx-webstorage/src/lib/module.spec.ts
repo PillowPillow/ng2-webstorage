7e07ff79e5e368e221527a660b26e9a3bf9314b8
PillowPillow <gaignoux.nicolas@gmail.com>
2018-11-07 11:51:17 +0100

    add test decorator example with custom strategy stub

--- projects/ngx-webstorage/src/lib/module.spec.ts
+++ projects/ngx-webstorage/src/lib/module.spec.ts
@@ -1,14 +1,25 @@
-import {inject, TestBed} from '@angular/core/testing';
+import {ComponentFixture, inject, TestBed} from '@angular/core/testing';
 import {NgxWebstorageModule} from './module';
 import {StrategyIndex} from './services/strategyIndex';
 import {STORAGE_STRATEGIES} from './strategies';
 import {StorageStrategy} from './core/interfaces/storageStrategy';
 import {StorageStrategyStub} from '../stubs/storageStrategy.stub';
 import {LocalStorageStrategy} from './strategies/localStorage';
+import {Component} from '@angular/core';
+import {LocalStorage} from './decorators';
+import {StorageKeyManager} from './helpers/storageKeyManager';
 
 describe('Module', () => {
 
 	let strategyStub: StorageStrategy<any>;
+
+	@Component({selector: 'mock', template: ''})
+	class LocalMockComponent {
+		@LocalStorage() prop: string;
+	}
+
+	let testFixture: ComponentFixture<LocalMockComponent>;
+	let testComponent: LocalMockComponent;
 
 	beforeEach(() => {
 		strategyStub = new StorageStrategyStub(LocalStorageStrategy.strategyName);
@@ -19,8 +30,13 @@ describe('Module', () => {
 			],
 			providers: [
 				{provide: STORAGE_STRATEGIES, useFactory: () => strategyStub, multi: true},
-			]
-		});
+			],
+			declarations: [
+				LocalMockComponent,
+			],
+		}).compileComponents();
+		testFixture = TestBed.createComponent(LocalMockComponent);
+		testComponent = testFixture.debugElement.componentInstance;
 	});
 
 	it('should index the storage strategies', inject([], () => {
@@ -32,5 +48,14 @@ describe('Module', () => {
 		expect(strategy).toEqual(strategyStub);
 	}));
 
+	it('should access to the stub storage strategy by using decorators', inject([StrategyIndex], (index: StrategyIndex) => {
+		const strategy: StorageStrategyStub = index.getStrategy(LocalStorageStrategy.strategyName) as StorageStrategyStub;
+		const propName: string = StorageKeyManager.normalize('prop');
+		expect(strategy.store[propName]).toBeUndefined();
+		testComponent.prop = 'foobar';
+
+		expect(testComponent.prop).toEqual('foobar');
+		expect(strategy.store[propName]).toEqual('foobar');
+	}));
 });
 
