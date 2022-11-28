9d5bb80f66fef73d62bc03b3ec783095786a02f2
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2022-11-28 16:57:11 +0100

    wip v15

--- projects/ngx-webstorage/src/test.ts
+++ projects/ngx-webstorage/src/test.ts
@@ -5,8 +5,6 @@ import 'zone.js/testing';
 import {getTestBed} from '@angular/core/testing';
 import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
 
-declare const require: any;
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
 	BrowserDynamicTestingModule,
@@ -14,7 +12,3 @@ getTestBed().initTestEnvironment(
     teardown: { destroyAfterEach: false }
 }
 );
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
