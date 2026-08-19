63282ed257942edc76d0f32c51904a9bf520865d
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2026-08-19 15:24:54 +0200

    fix(sandbox): migrate karma conf to karma-coverage
    src/karma.conf.js still required karma-coverage-istanbul-reporter,
    removed from devDependencies by this PR, so `npm test` on the root
    project crashed MODULE_NOT_FOUND at config load. CI never exercises
    this config (it always names the two lib projects). Mirrors the lib
    karma confs.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -9,15 +9,18 @@ module.exports = function(config) {
 			require('karma-jasmine'),
 			require('karma-chrome-launcher'),
 			require('karma-jasmine-html-reporter'),
-			require('karma-coverage-istanbul-reporter')
+			require('karma-coverage')
 		],
 		client: {
 			clearContext: false // leave Jasmine Spec Runner output visible in browser
 		},
-		coverageIstanbulReporter: {
+		coverageReporter: {
 			dir: require('path').join(__dirname, '../coverage'),
-			reports: ['html', 'lcovonly'],
-			fixWebpackSourcePaths: true
+			subdir: '.',
+			reporters: [
+				{ type: 'html' },
+				{ type: 'lcovonly' }
+			]
 		},
 		customLaunchers: {
 			chrome_headless: {
